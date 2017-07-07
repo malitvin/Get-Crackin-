ad0a4d7382a3f585695983299784377d1ea165ff
Maxim Litvinov <maximlitvinov@archiemd.com>
2017-07-07 11:09:30 -0400

    UI events and number sprites

---
+++ Assets/Scripts/UI/Framework/UIEvents.cs
@@ -0,0 +1,16 @@
+﻿//Unity
+
+namespace UI.Framework
+{
+    /// <summary>
+    /// Different types of things that can happen related to the UI
+    /// This is so that the UI and gameplay state machine states can work together without knowing much about each other
+    /// </summary>
+    public class UIEvents
+    {
+        public enum Type
+        { 
+            Test
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Framework/UIObserver.cs
@@ -0,0 +1,95 @@
+﻿//Unity
+using UnityEngine;
+using UnityEngine.Events;
+
+//Project
+
+//C#
+using System;
+using System.Collections.Generic;
+
+namespace UI.Framework
+{
+    /// <summary>
+    /// Observer Pattern in a way
+    /// http://gameprogrammingpatterns.com/observer.html
+    /// </summary>
+    public class UIObserver : MonoBehaviour
+    {
+
+        private Dictionary<UIEvents.Type, Action<string>> eventDictionary;
+
+        /// <summary>
+        /// For Performance to avoid boxing
+        /// </summary>
+        public struct MyEnumComparer : IEqualityComparer<UIEvents.Type>
+        {
+            public bool Equals(UIEvents.Type x, UIEvents.Type y)
+            {
+                return x == y;
+            }
+
+            public int GetHashCode(UIEvents.Type obj)
+            {
+                return (int)obj;
+            }
+        }
+
+        void Init()
+        {
+            if (eventDictionary == null)
+            {
+                eventDictionary = new Dictionary<UIEvents.Type, Action<string>>(new MyEnumComparer());
+            }
+        }
+
+        public void StartListening(UIEvents.Type eventName, Action<string> listener)
+        {
+            if (eventDictionary == null) Init();
+
+            Action<string> thisEvent = null;
+            if (eventDictionary.ContainsKey(eventName))
+            {
+                thisEvent = eventDictionary[eventName];
+                thisEvent += listener;
+            }
+            else
+            {
+                thisEvent += listener;
+                eventDictionary.Add(eventName, thisEvent);
+            }
+        }
+
+        public void StopListening(UIEvents.Type eventName, Action<string> listener)
+        {
+            if (eventDictionary == null) Init();
+
+            Action<string> thisEvent = null;
+            if (eventDictionary.ContainsKey(eventName))
+            {
+                thisEvent = eventDictionary[eventName];
+                thisEvent -= listener;
+            }
+        }
+
+        public void TriggerEvent(UIEvents.Type eventName,string message="")
+        {
+            if (eventDictionary == null) Init();
+
+            Action<string> thisEvent = null;
+
+            if (eventDictionary.ContainsKey(eventName))
+            {
+                thisEvent = eventDictionary[eventName];
+                thisEvent(message);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if(eventDictionary != null) eventDictionary.Clear();
+            eventDictionary = null;
+        }
+
+    }
+}
