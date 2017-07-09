91eb3834022c04a0c11f3b158021d8df39387be1
Maximus <malitvin@ncsu.edu>
2017-07-09 00:26:50 -0400

    safe assets

---
+++ Assets/Scripts/Common/UI/Effects/CasualRotate.cs
@@ -0,0 +1,33 @@
+﻿//Unity
+using UnityEngine;
+
+namespace Common.UI.Effects
+{
+
+    public class CasualRotate : MonoBehaviour
+    {
+        
+        public enum EulerAxis { X,Y,Z}
+
+        public float rotateSpeed;
+
+        public EulerAxis rotationAxis;
+
+        private void Update()
+        {
+            switch(rotationAxis)
+            {
+                case EulerAxis.X:
+                    transform.eulerAngles += new Vector3(rotateSpeed * Time.deltaTime, 0,0);
+                    break;
+                case EulerAxis.Y:
+                    transform.eulerAngles += new Vector3(0, rotateSpeed * Time.deltaTime, 0);
+                    break;
+                case EulerAxis.Z:
+                    transform.eulerAngles += new Vector3(0, 0, rotateSpeed * Time.deltaTime);
+                    break;
+            }
+            
+        }
+    }
+}
