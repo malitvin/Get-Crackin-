9190e77cbb77a3d16d5b840e002e2b5e24b4c248
Maxim Litvinov <maximlitvinov@archiemd.com>
2017-07-25 09:52:27 -0400

    git

---
+++ Assets/Scripts/Database/Secrets/SecretKEYS.cs
@@ -0,0 +1,13 @@
+﻿namespace Database
+{
+    public static class SecretKEYS
+    {
+        public static string URL = "http://dreamlo.com/lb/lGeAqCRwgkySAAWwipzVzALlgDAJ2b4UuzPRCjtBWbAg";
+        public static string publicKEY = "lGeAqCRwgkySAAWwipzVzALlgDAJ2b4UuzPRCjtBWbAg";
+        public static string privateKEY = "59774611b0b05a1ad4c01796";
+
+        
+
+
+    }
+}
