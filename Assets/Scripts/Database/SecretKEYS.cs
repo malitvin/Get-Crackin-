b40c88f45b3b1f55a52933426592539a94ed30e9
Maxim Litvinov <maximlitvinov@archiemd.com>
2017-07-25 09:30:48 -0400

    secret keys

---
+++ Assets/Scripts/Database/SecretKEYS.cs
@@ -0,0 +1,9 @@
+﻿namespace Database
+{
+    public static class SecretKEYS
+    {
+        public static string URL = "";
+        public static string publicKEY = "";
+        public static string privateKEY = "";
+    }
+}
